b336fb01c8def7f3ed21283fb0b592a7bd03e0e0
Mike Rybka <merybka@gmail.com>
2025-06-15 08:44:03 -0400

    Handle gh webhooks

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/mikerybka/util"
+)
+
+func main() {
+	http.HandleFunc("POST /api/webhooks/github", func(w http.ResponseWriter, r *http.Request) {
+		io.Copy(os.Stdout, r.Body)
+		os.Stdout.Write([]byte("\n"))
+	})
+	port := util.EnvVar("PORT", "3000")
+	http.ListenAndServe(":"+port, nil)
+}
