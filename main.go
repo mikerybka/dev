7e3f287337ab16b8d9288c626bab545e7d727d3e
Mike Rybka <merybka@gmail.com>
2025-07-06 19:12:47 -0400

    😬

--- main.go
+++ main.go
@@ -4,17 +4,27 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"os"
+	"os/exec"
 
 	"github.com/mikerybka/util"
 )
 
 func main() {
-	http.HandleFunc("POST /api/webhooks/github", func(w http.ResponseWriter, r *http.Request) {
-		io.Copy(os.Stdout, r.Body)
-		os.Stdout.Write([]byte("\n"))
+	http.HandleFunc("POST /api/eval", func(w http.ResponseWriter, r *http.Request) {
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+		cmd := exec.Command("/bin/bash", "-c", string(b))
+		cmd.Stdout = w
+		cmd.Stderr = w
+		err = cmd.Run()
+		if err != nil {
+			fmt.Println(err)
+			panic(err)
+		}
 	})
-	port := util.EnvVar("PORT", "3000")
+	port := util.RequireEnvVar("PORT")
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		fmt.Println(err)
