3ca1ce0dde2749e75b1f75089f8d827e9ed4fe00
Mike Rybka <merybka@gmail.com>
2025-07-07 18:27:39 -0400

    Create Client

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -11,49 +12,80 @@ import (
 	"github.com/mikerybka/util"
 )
 
-func main() {
-	http.HandleFunc("/btc", func(w http.ResponseWriter, r *http.Request) {
-		// RPC credentials
-		username := util.RequireEnvVar("BTCRPC_USER")
-		password := util.RequireEnvVar("BTCRPC_PASS")
+type Client struct {
+	User string
+	Pass string
+}
 
-		// JSON RPC payload
-		data := `{
+type GetBlockchainInfoResponse struct {
+	Result *BlockchainInfo `json:"result"`
+}
+
+type BlockchainInfo struct {
+	VerificationProgress float64 `json:"verificationprogress"`
+}
+
+func (c *Client) GetBlockchainInfo() (*GetBlockchainInfoResponse, error) {
+	// JSON RPC payload
+	data := `{
 		"jsonrpc": "1.0",
 		"id": "curltest",
 		"method": "getblockchaininfo",
 		"params": []
 	}`
 
-		req, err := http.NewRequest("POST", "http://127.0.0.1:8332", bytes.NewBuffer([]byte(data)))
-		if err != nil {
-			panic(err)
+	req, err := http.NewRequest("POST", "http://127.0.0.1:8332", bytes.NewBuffer([]byte(data)))
+	if err != nil {
+		panic(err)
+	}
+
+	// Set headers
+	req.Header.Set("Content-Type", "text/plain")
+
+	// Set basic auth
+	auth := c.User + ":" + c.Pass
+	authHeader := "Basic " + base64.StdEncoding.EncodeToString([]byte(auth))
+	req.Header.Set("Authorization", authHeader)
+
+	// Do the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Read the response
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &GetBlockchainInfoResponse{}
+	err = json.Unmarshal(body, res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func main() {
+	http.HandleFunc("/btc", func(w http.ResponseWriter, r *http.Request) {
+		// RPC credentials
+		username := util.RequireEnvVar("BTCRPC_USER")
+		password := util.RequireEnvVar("BTCRPC_PASS")
+		client := &Client{
+			User: username,
+			Pass: password,
 		}
 
-		// Set headers
-		req.Header.Set("Content-Type", "text/plain")
-
-		// Set basic auth
-		auth := username + ":" + password
-		authHeader := "Basic " + base64.StdEncoding.EncodeToString([]byte(auth))
-		req.Header.Set("Authorization", authHeader)
-
-		// Do the request
-		client := &http.Client{}
-		resp, err := client.Do(req)
+		res, err := client.GetBlockchainInfo()
 		if err != nil {
-			panic(err)
-		}
-		defer resp.Body.Close()
-
-		// Read the response
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			panic(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 
-		// Print the result
-		w.Write(body)
+		json.NewEncoder(w).Encode(res)
 	})
 
 	http.HandleFunc("POST /api/eval", func(w http.ResponseWriter, r *http.Request) {
