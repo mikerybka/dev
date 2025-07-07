70e8dfb955c81b6d1050d6ddd01280c82072962d
Mike Rybka <merybka@gmail.com>
2025-07-07 18:09:08 -0400

    Add /btc

--- go.mod
+++ go.mod
@@ -5,6 +5,18 @@ go 1.23.5
 require github.com/mikerybka/util v0.0.0-20250612144308-79c8fd3c02d9
 
 require (
+	github.com/btcsuite/btcd/btcec/v2 v2.1.3 // indirect
+	github.com/btcsuite/btcd/btcutil v1.1.5 // indirect
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0 // indirect
+	github.com/btcsuite/btclog v0.0.0-20170628155309-84c8d2346e9f // indirect
+	github.com/btcsuite/go-socks v0.0.0-20170105172521-4720035b7bfd // indirect
+	github.com/btcsuite/websocket v0.0.0-20150119174127-31079b680792 // indirect
+	github.com/decred/dcrd/crypto/blake256 v1.0.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+)
+
+require (
+	github.com/btcsuite/btcd v0.24.2
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/kr/fs v0.1.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -3,13 +3,37 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os/exec"
 
+	"github.com/btcsuite/btcd/rpcclient"
 	"github.com/mikerybka/util"
 )
 
 func main() {
+	http.HandleFunc("/btc", func(w http.ResponseWriter, r *http.Request) {
+		connCfg := &rpcclient.ConnConfig{
+			Host:         "localhost:8332",
+			User:         util.RequireEnvVar("BTCRPC_USER"),
+			Pass:         util.RequireEnvVar("BTCRPC_PASS"),
+			HTTPPostMode: true, // Bitcoin core only supports HTTP POST
+			DisableTLS:   true, // If TLS is not configured
+		}
+
+		client, err := rpcclient.New(connCfg, nil)
+		if err != nil {
+			log.Fatalf("Error creating new client: %v", err)
+		}
+		defer client.Shutdown()
+
+		res, err := client.GetBlockChainInfo()
+		if err != nil {
+			log.Fatalf("Error getting blockchain info: %v", err)
+		}
+
+		fmt.Fprintln(w, res.VerificationProgress)
+	})
 	http.HandleFunc("POST /api/eval", func(w http.ResponseWriter, r *http.Request) {
 		b, err := io.ReadAll(r.Body)
 		if err != nil {
