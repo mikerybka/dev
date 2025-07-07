a479e006cb1e0d0b7c23753beb5d4642098f9d8e
Mike Rybka <merybka@gmail.com>
2025-07-07 18:20:13 -0400

    Remove client library

--- main.go
+++ main.go
@@ -1,39 +1,61 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os/exec"
 
-	"github.com/btcsuite/btcd/rpcclient"
 	"github.com/mikerybka/util"
 )
 
 func main() {
 	http.HandleFunc("/btc", func(w http.ResponseWriter, r *http.Request) {
-		connCfg := &rpcclient.ConnConfig{
-			Host:         "localhost:8332",
-			User:         util.RequireEnvVar("BTCRPC_USER"),
-			Pass:         util.RequireEnvVar("BTCRPC_PASS"),
-			HTTPPostMode: true, // Bitcoin core only supports HTTP POST
-			DisableTLS:   true, // If TLS is not configured
-		}
+		// RPC credentials
+		username := util.RequireEnvVar("BTCRPC_USER")
+		password := util.RequireEnvVar("BTCRPC_PASS")
 
-		client, err := rpcclient.New(connCfg, nil)
+		// JSON RPC payload
+		data := `{
+		"jsonrpc": "1.0",
+		"id": "curltest",
+		"method": "getblockchaininfo",
+		"params": []
+	}`
+
+		req, err := http.NewRequest("POST", "http://127.0.0.1:8332", bytes.NewBuffer([]byte(data)))
 		if err != nil {
-			log.Fatalf("Error creating new client: %v", err)
+			panic(err)
 		}
-		defer client.Shutdown()
 
-		res, err := client.GetBlockChainInfo()
+		// Set headers
+		req.Header.Set("Content-Type", "text/plain")
+
+		// Set basic auth
+		auth := username + ":" + password
+		authHeader := "Basic " + base64.StdEncoding.EncodeToString([]byte(auth))
+		req.Header.Set("Authorization", authHeader)
+
+		// Do the request
+		client := &http.Client{}
+		resp, err := client.Do(req)
 		if err != nil {
-			log.Fatalf("Error getting blockchain info: %v", err)
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		// Read the response
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
 		}
 
-		fmt.Fprintln(w, res.VerificationProgress)
+		// Print the result
+		w.Write(body)
 	})
+
 	http.HandleFunc("POST /api/eval", func(w http.ResponseWriter, r *http.Request) {
 		b, err := io.ReadAll(r.Body)
 		if err != nil {
